979ff7d83a1a0e5e3a0c4e41710eeb0f9d097c98
Zach Smith <x.coder.zach@gmail.com>
2013-11-11 13:37:22 -0500

    initial commit

---
+++ Readme.md
@@ -0,0 +1,18 @@
+
+# transition-router
+
+  pushState router with css transitions
+
+## Installation
+
+  Install with [component(1)](http://component.io):
+
+    $ component install xcoderzach/transition-router
+
+## API
+
+
+
+## License
+
+  MIT
---
+++ test/index.js
@@ -0,0 +1,70 @@
+(function() {
+  var Router = require('transition-router')
+    , router = new Router
+    , container = document.getElementById('container')
+
+  describe('when I setup two routes', function() {
+    before(function() {
+      var that = this
+      router.page('/first', { column: 0 }, function() {
+        console.log('wat')
+        that.firstEl = this.el = document.createElement('div')
+        container.appendChild(this.el)
+      })
+      router.page('/second', { column: 1 }, function() {
+        that.secondEl = this.el = document.createElement('div')
+        container.appendChild(this.el)
+      })
+      router.go('/first')
+    })
+
+    after(function() {
+      router.go('/test/index.html')
+    })
+    it('should not start the animation syncronously', function() {
+      expect(this.firstEl.classList.contains('current-page')).to.be.ok
+    })
+    it('should transition in the right direction', function(done) {
+      var that = this
+      router.go('/second')
+      that.firstEl.addEventListener('webkitAnimationStart', function check1() {
+        that.firstEl.removeEventListener('webkitAnimationStart', check1)
+        expect(that.firstEl.classList.contains('to-left')).to.be.true
+        expect(that.secondEl.classList.contains('from-right')).to.be.true
+
+        expect(that.firstEl.classList.contains('current-page')).to.be.true
+        expect(that.secondEl.classList.contains('current-page')).to.be.true
+
+        router.go('/first')
+        that.firstEl.addEventListener('webkitAnimationStart', function check2() {
+          that.firstEl.removeEventListener('webkitAnimationStart', check2)
+          expect(that.firstEl.classList.contains('from-left')).to.be.true
+          expect(that.secondEl.classList.contains('to-right')).to.be.true
+
+          expect(that.secondEl.classList.contains('current-page')).to.be.true
+          expect(that.firstEl.classList.contains('current-page')).to.be.true
+          done()
+        })
+      })
+    })
+    it('should call the second callback once the transitions are done', function(done) {
+      var that = this
+      router.page('/third', { column: 3 }, function() {
+        that.thirdEl = this.el = document.createElement('div')
+        container.appendChild(this.el)
+      }, function() {
+        expect(that.thirdEl.classList.contains('current-page')).to.be.true
+        expect(that.firstEl.classList.contains('current-page')).to.be.false
+        expect(that.secondEl.classList.contains('current-page')).to.be.false
+
+        expect(that.thirdEl.classList.length).to.equal(1)
+        expect(that.firstEl.classList.length).to.equal(0)
+        expect(that.secondEl.classList.length).to.equal(0)
+
+        done()
+      })
+      router.go('/third')
+    })
+  })
+
+}())
